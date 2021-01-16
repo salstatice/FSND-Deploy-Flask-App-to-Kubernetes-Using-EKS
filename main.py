b120b8dbe7ed92a36614d8ba9bb1120ecf27bcb5
Unkei Kou <unkei.kou.chs@gmail.com>
2021-01-15 23:50:37 -0500

    Trigger build attempt#2

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from flask import Flask, jsonify, request, abort
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
-myVar="update the build"
 
 def _logger():
     '''
